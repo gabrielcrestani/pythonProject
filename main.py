512260f83a441921e146321c6e45f2d7538181bd
gabrielcrestani <70517063+gabrielcrestani@users.noreply.github.com>
2022-01-20 09:15:38 -0300

    Alterações da 2ª aula do curso com a estrutura do While, e uso do if-elif-else como um swith-case ou Match-Case

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 
 
-
 def print_hi(name):
 
     print(f'Oi, {name}')
     print('Oi, '+ name)
 
 def calcular_area_do_retangulo(largura, comprimento):
-
     return largura * comprimento
 
 def calcular_area_do_quadrado(lado):
@@ -16,7 +14,7 @@ def calcular_area_do_quadrado(lado):
 def calcular_area_do_triangulo(largura,comprimento):
     return largura * comprimento / 2
 
-def contagem_regressiva(fim):
+def contagem_progressiva(fim):
     for numero in range(fim):   # repetir o bloco de zero até o fim
         print(numero)           # exibir o número
 
@@ -40,31 +38,53 @@ def brincar_de_plim(fim):
             print('{:0>3}'.format(numero))
 
 
-
 # estrutura de identificação / execução do script
 if __name__ == '__main__':
-    print_hi('Gabriel')
 
-#chamar a área do retângulo
+    resposta = 'C'
+    while resposta.upper() != 'Z':
+        print('###############################################')
+        print('#                                             #')
+        print('#           M E N U D E O P Ç Õ E S           #')
+        print('#                                             #')
+        print('#           1 - Olá Mundo                     #')
+        print('#           2 - Área do Retângulo             #')
+        print('#           3 - Área do Quadrado              #')
+        print('#           4 - Área do Triângulo             #')
+        print('#           5 - Contagem Progressiva          #')
+        print('#           6 - Apoiar Candidato              #')
+        print('#           7 - Brincar de Plim               #')
+        print('#                                             #')
+        print('#           Z - Sair                          #')
+        print('#                                             #')
+        print('###############################################')
 
-resultado = calcular_area_do_retangulo(3,4)
-print(f'A área do retângulo é de {resultado} m²')
+        resposta = input("Escolha sua opção:")
+        print(f'A sua escolha foi: {resposta}')
 
-#chamar a função de cálculo da área do quadrado
-resultado = calcular_area_do_quadrado(5)
-print(f'A área do quadrado é de {resultado} m²')
+        if resposta.upper() != 'Z':
+            if resposta == '1':
+                print_hi('José')
+            elif resposta == '2':
+                resultado = calcular_area_do_retangulo(8,7)
+                print(f'A área do retângulo é de {resposta} m²')
+            elif resposta == '3':
+                resultado = calcular_area_do_quadrado(6)
+                print(f'A área do quadrado é de {resposta} m²')
+            elif resposta == '4':
+                resultado = calcular_area_do_triangulo(5,8)
+                print(f'A área do triângulo é de {resposta} m²')
+            elif resposta == '5':
+                contagem_progressiva(10)
+            elif resposta == '6':
+                apoiar_candidato('Murphy',13)
+            elif resposta == '7':
+                brincar_de_plim(7)
+            else:
+                print('Você digitou uma opção inválida . Escolha uma opção de 1 a 7')
+        else:
+            print('Você escolheu sair. Volte Sempre!')
 
-#chamar a função de cálculo da área do triangulo
-resultado = calcular_area_do_triangulo(6,7)
-print(f'A área do triângulo é de {resultado} m²')
 
-#executar uma contagem progressiva
-contagem_regressiva(11)
-
-#exibir o nome do candidato
-apoiar_candidato('Faker',100)
-
-#brincar de plim
-brincar_de_plim(100)
 
 
